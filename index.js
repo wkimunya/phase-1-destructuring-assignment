c0ef919418ad9901e2d55c312ebee30f8db08383
Wilson Kimunya <kimunya.wilson@gmail.com>
2023-07-19 21:58:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const farmAnimals = 'cow horse sheep pig chicken';
+// 1. Use destructuring to assign appropriate variables based on the sounds animals make.
 
+const farmAnimals = 'cow horse sheep pig chicken'; 
+const[moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+console.log(moo, neigh, baa, oink, cluck);
+
+// 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+const[bessie, , dolly, babe, little]= farmAnimals.split(' ');
+
+//3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+const[blackAndWhite, ,black, pink, ,]= farmAnimals.split(' ');
+
+// 4. Use destructuring to assign appropriate variables using the color names.
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const[red, orange, yellow,green, blue, indigo, violet]= colors;
+console.log(red, orange, yellow,green, blue, indigo, violet);
 
+// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+const[r,o,y,g,b, , v]= colors;
+
+// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+const[,,,,,indg,]= colors;
+
+// 7. Use destructuring to assign all variables using the keys as the variable names
 const muppet = {
   muppetName: 'Miss Piggy',
   color: 'pink',
@@ -9,7 +29,9 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+const{muppetName, color, song, job, partner}= muppet;
 
+// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
 const nestedMuppet = {
   nestedName: 'Kermit',
   nestedColor: 'green',
@@ -24,6 +46,13 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+const{
+  album: {
+    theMuppetMovie: {song2,song4}
+  },
+  nestedJob,
+  nestedPartner
+}= nestedMuppet;
 
 // Strings
 
